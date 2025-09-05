4747c38725698b60912c86f7d797cafe8ab3cb65
ravinangare <73210215+ravinangare@users.noreply.github.com>
2025-09-05 08:04:02 +0530

    Update LanguagePageTest.spec.js

--- LanguagePageTest.spec.js
+++ LanguagePageTest.spec.js
@@ -2,7 +2,6 @@ import { test, expect } from '@playwright/test';
 
 test('test', async ({ page }) => {
   await page.goto('https://www.amazon.in/');
-//  await page.getByRole('button', { name: 'Continue shopping' }).click();
   await expect(page.getByRole('link', { name: 'Choose a language for shopping in Amazon India. The current selection is' })).toBeVisible();
   await page.getByRole('link', { name: 'Choose a language for shopping in Amazon India. The current selection is' }).click();
   await page.locator('label').filter({ hasText: 'हिन्दी - HI - अनुवाद' }).locator('i').click();
